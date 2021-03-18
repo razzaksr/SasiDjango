e2984fc73fb8d76a16d7203e7b5420067fe2ad08
razzaksr <razzaksr@gmail.com>
2021-03-18 12:27:16 +0530

    Jinja and Forming

--- SasiProject/Intro/urls.py
+++ SasiProject/Intro/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.greet,name='demo-page'),
     path('hai', views.hello, name='DTL-page'),
+    path('type',views.typeForm,name='form'),
+    path('hey',views.respForm,name='respond'),
 ]
--- SasiProject/Intro/views.py
+++ SasiProject/Intro/views.py
@@ -7,4 +7,13 @@ def greet(request):
     return HttpResponse("<h1>Zealous Welcomes you to learn DJANGO</h1>")
 
 def hello(request):
-    return render(request,'hai.html')
+    return render(request,'hai.html',{'domain':'Flask'})
+
+def typeForm(request):
+    return render(request,'Forming.html')
+
+def respForm(request):
+    one=int(request.POST['alpha'])
+    two = int(request.POST['beta'])
+    res=one*two
+    return render(request,'check.html',{'result':res})
